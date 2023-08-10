3054133dc6d7b7750b1c9bee194fd6964c6cc125
Aidana <aassarbayeva@gmail.com>
2023-08-10 16:27:43 +0600

    Initial Commit

---
+++ firstappka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstappka
+//
+//  Created by Aidana Assarbayeva on 10.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
